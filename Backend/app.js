206d0688097a255978eddfac6088e3a19fa56a8f
erik forero <erikillzon@gmail.com>
2022-12-02 18:18:20 -0500

    making a working starting code

--- Backend/app.js
+++ Backend/app.js
@@ -1,1 +1,10 @@
- console.log("nodemon si funciona!");
+import express from "express";
+
+const app = express();
+const port = 8080;
+//middleware
+app.use(express.json());
+//listener para inicar el server
+app.listen(port,()=>{
+    console.log("conexion started at:"+ port);
+});
