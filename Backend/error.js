a25ff927f9700da453db9eccc39a491b5103f98e
Miguel Castro <miguelangelcmoreno@gmail.com>
2022-12-01 19:57:33 -0500

    Updtate gitignore

--- Backend/error.js
+++ Backend/error.js
@@ -1,2 +1,1 @@
-//Mensaje de error
 console.error("b");
--- Frontend/error.js
+++ Frontend/error.js
@@ -1,9 +1,3 @@
 console.error("f");
-console.error("This is erik's erroar");
- "Erik estuvo ahi"
- function git(a,b){
-   return a+b;
- }
 
-git();
 
