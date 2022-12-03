28370037effd1366243f2bc276730dcfd3a541cf
Miguel Castro <miguelangelcmoreno@gmail.com>
2022-12-02 21:02:39 -0500

    Conection to MongoDB Cluster

--- Backend/app.js
+++ Backend/app.js
@@ -2,10 +2,20 @@ import express from "express";
 import mongoose from "mongoose";
 
 const app = express();
-const port = process.env.PORT || 8080;
-//middleware
-app.use(express.json());
-//listener para inicar el server
-app.listen(port,()=>{
-    console.log("conexion started at:"+ port);
+const puerto = process.env.PORT || 8080;
+
+// Listener para iniciar el servidor
+
+app.listen(puerto, () => {
+  console.log(`Servidor ejecutado correctamente: ${puerto}`);
 });
+
+// Conexión al cluster
+
+mongoose.connect("mongodb+srv://admin:Soyeladmin1@chatucluster.3gjh3iz.mongodb.net/Chatu?retryWrites=true&w=majority", (err) => {
+  console.log(err ? `❌ ERROR DE CONEXIÓN CON EL CLUSTER: ${err}` : "✅ La base de datos esta conectada");
+});
+
+// Middlewares
+
+app.use(express.json());
