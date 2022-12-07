5feebb83a6b393d2ced95dbae7ac857496cd68e2
Miguel Castro <miguelangelcmoreno@gmail.com>
2022-12-07 09:06:34 -0500

    Removing browser settings

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,7 @@
-console.log("Ivon");
+import React from "react";
+import ReactDOM from "react-dom";
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+
+root.render(<h1>Hola como estas</h1>);
