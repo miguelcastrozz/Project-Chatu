3ca2118c58221918c1a27e1815a1b511cc4aa266
Miguel Castro <miguelangelcmoreno@gmail.com>
2022-12-14 16:16:14 -0500

    After 1 hour we finally fixed a router error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,23 +3,16 @@ import { Link, Route, Routes } from "react-router-dom";
 
 import "./style-sheets/root/global.css";
 import "./style-sheets/root/reset.css";
-import Landing from "./components/landing/landingPage";
+
+import Inicio from "./views/Incio";
 
 export default function App() {
   return (
-    <nav>
-      <Link to="/" />
-      <Link to="/login" />
-      <Link to="/registro" />
-      <Link to="/publicaciones" />
-      <Link to="/comentarios" />
-      <Routes>
-        <Route path="/" element={<Landing />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="/registro" element={<Registro />}></Route>
-        <Route path="/publicaciones" element={<Publicaciones />}></Route>
-        <Route path="/comentarios" element={<Comentarios />}></Route>
-      </Routes>
-    </nav>
+      <div>
+          <Routes>
+            <Route path="/" element={<Inicio />}></Route>
+            <Route path ='*' element={<h1>404 NOT FOUND</h1>}></Route>
+          </Routes>
+      </div>
   );
 }
