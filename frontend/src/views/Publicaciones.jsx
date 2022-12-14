ff483317cc5a83e4f962898d41761b9bfbc08161
MANRED12 <roral@hotmail.com>
2022-12-14 13:17:53 -0500

    front publicaciones

--- frontend/src/views/Publicaciones.jsx
+++ frontend/src/views/Publicaciones.jsx
@@ -1,6 +1,16 @@
-export default function Publicaciones() {
-  return (
-    <>
-    </>
-  );
+import React from "react";
+
+import { useParams } from "react-router-dom";
+
+function publicacion(){
+
+
+  return(
+    <h1>
+      Estas viendo publicaciones
+    </h1>
+  )
+
 }
+
+export default publicacion
