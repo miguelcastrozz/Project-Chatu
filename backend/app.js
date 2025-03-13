3ee0e324cf868e689b0e4f08702d13aea29081d2
Miguel Castro <miguelangelcmoreno@gmail.com>
2025-03-13 17:59:17 -0500

    Add dotenv for environment variable management and update MongoDB connection string

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,18 @@ import mongoose from "mongoose";
 
 import apiRouter from "./routes/apiRouter.js";
 
+import { config } from "dotenv";
+
 const app = express();
 const port = process.env.port || 8080;
+
+config();
 
 app.listen(port, () => {
   console.log(`Servidor ejecutado exitosamente: ${port}`);
 });
 
-mongoose.connect("mongodb+srv://admin:Soyeladmin1@chatucluster.3gjh3iz.mongodb.net/Chatu?retryWrites=true&w=majority", (err) => {
+mongoose.connect(process.env.DB_URI, (err) => {
   console.log(err ? `Error de conexión con el cluster: ${err}` : "Base de datos conectada exitosamente");
 });
 
